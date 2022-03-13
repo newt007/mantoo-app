9c4eb65a884956bf7cd834f6c605cbea53752fb2
Bintang Putera <sangbintang917@gmail.com>
2022-03-13 17:37:10 +0700

    Initial commit

---
+++ app/src/main/java/com/elapp/mantuapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.elapp.mantuapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
